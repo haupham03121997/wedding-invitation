bd76b0889fae1a6da8c37104c5b643ee8c3d4110
Adobe Pham <xuanhau031297@gmail.com>
2025-10-29 23:46:22 +0700

    Update font styles in globals.css and layout.tsx to use Montserrat and Playfair Display. Removed Geist fonts and adjusted font-family properties accordingly.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,17 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono, Playfair_Display } from 'next/font/google';
+import { Montserrat, Playfair_Display } from 'next/font/google';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 const playfairDisplay = Playfair_Display({
   variable: '--font-playfair',
   subsets: ['latin'],
   weight: ['400', '600', '700'],
+});
+
+const montserrat = Montserrat({
+  variable: '--font-montserrat',
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
@@ -30,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} ${playfairDisplay.variable} antialiased`}>
-        {children}
-      </body>
+      <body className={`${playfairDisplay.variable} ${montserrat.variable} antialiased`}>{children}</body>
     </html>
   );
 }
